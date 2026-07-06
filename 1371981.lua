1e6ddcd57f35f6b9340a1723cedcfbb607f6e2fa
dc190 <114036261+dc190@users.noreply.github.com>
2026-07-06 19:00:40 +0800

    Update app 1371980: 3 depot(s)

---
+++ 1371981.lua
@@ -0,0 +1,5 @@
+--appid=1371980
+--depots
+addappid(1371981)
+--noupdate
+setManifestid(1371981,"510898126581031226")
---
+++ 1371982.lua
@@ -0,0 +1,5 @@
+--appid=1371980
+--depots
+addappid(1371982)
+--noupdate
+setManifestid(1371982,"8466480832192743958")
