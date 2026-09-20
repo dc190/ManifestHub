b84df1219860dfa920c28086259caaffbe8a7282
dc190 <114036261+dc190@users.noreply.github.com>
2026-09-20 14:23:47 +0800

    Update app 2483190: 1 depot(s)

--- 2483190.lua
+++ 2483190.lua
@@ -1,5 +1,5 @@
 --appid=2483190
 --depots
-addappid(2483191)
+addappid(2483191,1,"f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9")
 --noupdate
 setManifestid(2483191,"1681298474714072536")
